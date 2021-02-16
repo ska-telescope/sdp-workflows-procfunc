c36301f79263ccc9d309368c6a0564c83882a63f
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2021-02-16 16:06:24 +0000

    SIM-738: Port CBF-SDP receive to vis-receive workflow

--- src/workflows/vis_receive/vis_receive.py
+++ src/workflows/vis_receive/vis_receive.py
@@ -1,5 +1,7 @@
 """
-Example visibility receive workflow.
+Example receive workflow
+Ported CBF-SDP receive workflow
+Deploys and update receive addresses attribute with DNS-based IP address
 """
 
 # pylint: disable=invalid-name
@@ -10,20 +12,54 @@ from ska_sdp_workflow import workflow
 
 # Initialise logging
 ska.logging.configure_logging()
-log = logging.getLogger('vis_receive')
+log = logging.getLogger("vis_receive")
 log.setLevel(logging.INFO)
 
 # Claim processing block
 pb = workflow.ProcessingBlock()
+parameters = pb.get_parameters()
+
+log.info("Setting default parameters")
+values = {
+    "image": "nexus.engageska-portugal.pt/ska-docker/cbf_sdp_emulator",
+    "version": "latest",
+    "recv_emu": "emu-recv",
+    "model.name": "sim-vis.ms",
+    "reception.outputfilename": "output.ms",
+    "transmission.channels_per_stream": 4,
+    "transmission.rate": "147500",
+    "payload.method": "icd",
+    "reader.num_timestamps": 0,
+    "reader.start_chan": 0,
+    "reader.num_chan": 0,
+    "reader.num_repeats": 1,
+    "results.push": "false",
+    "pvc.name": "local-pvc",
+    "pvc.path": "/mnt/data",
+}
+
+# Override the defaults with values from the PB
+if parameters:
+    for param in parameters.keys():
+        log.info("Over-riding defaults with parameters from the PB")
+        values[param] = parameters.get(param)
 
 # Create work phase
 log.info("Create work phase")
-work_phase = pb.create_phase('Work', [])
+work_phase = pb.create_phase("Work", [])
 
+# Assuming we are only deploying one receive processes
 with work_phase:
 
     # Deploy visibility receive
-    work_phase.ee_deploy_helm('vis-receive')
+    work_phase.ee_deploy_helm("receive", values)
+
+    # Get the channel link map from SBI
+    scan_types = pb.get_scan_types()
+
+    # Add receive addresses to pb
+    pb.receive_addresses(scan_types)
+
     log.info("Done, now idling...")
 
     for txn in work_phase.wait_loop():
